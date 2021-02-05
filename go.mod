b1bf4d237916e027eab4f55ca4c9d9f2896b8704
Tenzin Sean <tenzin.jeunesse@gmail.com>
2021-02-05 21:18:33 +0100

    added

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.15
 require (
 	github.com/google/go-cmp v0.5.4
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/labstack/echo/v4 v4.1.17
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777
 )
--- hello.go
+++ hello.go
@@ -2,41 +2,65 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"reflect"
-	"runtime"
-	"github.com/julienschmidt/httprouter"
-        "example.com/user/hello/morestring"
-        "github.com/google/go-cmp/cmp"
+	//"net/http"
+	//"reflect"
+	//"runtime"
+	//"github.com/julienschmidt/httprouter"
+        //"example.com/user/hello/morestring"
+        //"github.com/google/go-cmp/cmp"
+    //"golang.org/x/net/http2"
+        "github.com/labstack/echo/v4"
+       "github.com/labstack/echo/v4/middleware"
 )
 
-func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", p.ByName("name"))
-}
+// type MyHandler struct {}
 
-func world(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello")
-}
+// func (h *MyHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello World!")
+// }
 
-func log(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
-		fmt.Println("Handler function called -" + name)
-		h(w, r)
-	}
-}
+// func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+// 	fmt.Fprintf(w, "hello, %s!\n", p.ByName("name"))
+// }
+
+// func world(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "Hello")
+// }
+
+// func log(h http.HandlerFunc) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+// 		fmt.Println("Handler function called -" + name)
+// 		h(w, r)
+// 	}
+// }
+
 
 func main() {
 
-        mux := httprouter.New()
-        mux.GET("/hello/:name", hello)
+        e := echo.New()
+        e.Use(middleware.Logger())
+        e.Use(middleware.Recover())
 
-	server := http.Server{
-		Addr: "127.0.0.1:8080",
-                Handler: mux,
-	}
-	//http.HandleFunc("/hello", hello)
-	server.ListenAndServe()
-        fmt.Println(morestrings.ReverseRunes("!oG, olleH"))
-	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+        e.GET("/", func(c echo.Context) error {
+		return c.File("index.html")
+	})
+
+        e.GET("/file", func(c echo.Context) error {
+		return c.File("echo.svg")
+	})
+
+
+        e.Logger.Fatal(e.Start(":1232"))
+	//server := http.Server{
+	//	Addr: "127.0.0.1:8080",
+	//}
+	//http.HandleFunc("/Headers", headers)
+	//server.ListenAndServe()
+
+    //     http2.ConfigureServer(&server, &http2.Server{})
+	// server.ListenAndServeTLS("cert.pem", "key.pem")
+        //fmt.Println(morestrings.ReverseRunes("!oG, olleH"))
+	//fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+         fmt.Println("hhiii")
 }
